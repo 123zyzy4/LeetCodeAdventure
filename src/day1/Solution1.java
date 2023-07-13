f0e6d98d664531c6b9b4db4e9422e5565857e07e
ZhangYu <2233790741@qq.com>
2023-07-13 14:37:29 +0800

    11

---
+++ src/day1/Solution1.java
@@ -0,0 +1,14 @@
+package day1;
+
+/**
+ * @ClassName Solution1
+ * @Description TODO
+ * @Author zhangyu
+ * @Date 2023/7/13 14:35
+ * @Version 1.0
+ */
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        return new int[0];
+    }
+}
