b4ea0fe0a0d000539f93a4f459fbd681d5e4a368
ZhangYu <2233790741@qq.com>
2023-08-19 15:15:03 +0800

    gittest

--- src/day8/Solution4.java
+++ src/day8/Solution4.java
@@ -34,6 +34,7 @@ public class Solution4 {
 
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
+        System.out.println("git1");
 
        if(list1==null||list2==null){
            return list1==null?list2:list1;
